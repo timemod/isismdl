1fc3ec75d8225806370357e9760cea0c638ce028
mdijkstra-cpb <m.dijkstra.cpb@gmail.com>
2017-03-20 15:21:22 +0100

    Fixed layout. Removed library()

--- pkg/R/solve_mdl.R
+++ pkg/R/solve_mdl.R
@@ -5,16 +5,14 @@ solve_mdl <- function(model_file, period, data, ca, fix_values, fit_targets) {
         stop("Please specify at least model_file, period and data.")
     }
 	
-	library(isismdl)
-	
     mdl <- isis_mdl(modelname = model_file, period = period, data = data)
 	
-	if (!missing(ca)) mdl$set_ca(ca)
-	if (!missing(fix_values)) mdl$set_fix(fixed_ts)
-	if (!missing(fit_targets)) mdl$set_fit(fit_targets)
-		
-	mdl$fill_mdl_data()
-	mdl$solve(period)
+    if (!missing(ca)) mdl$set_ca(ca)
+    if (!missing(fix_values)) mdl$set_fix(fixed_ts)
+    if (!missing(fit_targets)) mdl$set_fit(fit_targets)
+	
+    mdl$fill_mdl_data()
+    mdl$solve(period)
 
     return(mdl)
 }
