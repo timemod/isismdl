ef6f0ff6d95dbe43e45858d88b3bfce076e4a787
mdijkstra-cpb <m.dijkstra.cpb@gmail.com>
2017-03-23 13:21:43 +0100

    solve_mdl now returns only solved data and ca's. Intermediate solve information not shown (TODO).

--- pkg/R/solve_mdl.R
+++ pkg/R/solve_mdl.R
@@ -1,18 +1,25 @@
-#' Function solve_mdl returns a solved model, given a model file name and corresponding data.
+#' Function solve_mdl solves model for given data and returns resulting data and constant adjustments (ca).
 #' @export
-solve_mdl <- function(model_file, period, data, ca, fix_values, fit_targets) {
-    if (missing(model_file) || missing(period) || missing(data)) {
-        stop("Please specify at least model_file, period and data.")
+solve_mdl <- function(model_file, data, period, fix_values, ca, fit_targets) {
+    if (missing(model_file) || missing(data)) {
+        stop("Please specify at least model_file and data.")
     }
-	
-    mdl <- isis_mdl(model_file = model_file, period = period, data = data)
-	
+    
+    # Write output to temp file
+    temp <- file()
+	on.exit({sink(); close(temp)}, add = TRUE)
+    sink(temp)
+    
+    # Do the work
+    mdl <- isis_mdl(model_file = model_file, data = data)
+    
+    if (!missing(period)) mdl$set_period(period)
     if (!missing(ca)) mdl$set_ca(ca)
     if (!missing(fix_values)) mdl$set_fix(fix_values)
     if (!missing(fit_targets)) mdl$set_fit(fit_targets)
-	
+    
     mdl$fill_mdl_data()
-    mdl$solve(period)
+    mdl$solve()
 
-    return(mdl)
+    list(data = mdl$get_data()[period, drop = F], ca = mdl$get_ca()[period, drop = F])
 }
