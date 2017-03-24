4290f0e72a63f7ea060ab9426bc4f9e63416df6c
Rob van Harrevelt <rvh@cpb.nl>
2017-03-24 14:39:00 +0100

    The example job islm_filmdt.R now used the default report option in method fill_mdl_data.

--- examples/islm/islm_filmdt.R
+++ examples/islm/islm_filmdt.R
@@ -3,12 +3,12 @@ library(isismdl)
 
 rds_file <- "islm_basis.rds"
 if (!file.exists(rds_file)) {	
-    stop("No rds file with model present. Run job islm_basis.R first")
+  stop("No rds file with model present. Run job islm_basis.R first")
 }
 mdl <- read_mdl(rds_file)
 
 mdl$set_data(regts(NA, period = mdl$get_period()), names = "y")
 print(mdl$get_data())
-mdl$fill_mdl_data(report = "no");
+mdl$fill_mdl_data()
 print(mdl$get_data())
 
