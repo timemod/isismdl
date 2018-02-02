b83291fcb8d80d902565f3dc7449719bb719a899
Rob van Harrevelt <rvanharrevelt@gmail.com>
2018-02-02 14:56:49 +0100

    In .travis.yml the write permission on tests/testthat/misc/output is no longer set: this is probably not necessary.

--- pkg/tests/testthat/misc/helper_makedir.R
+++ pkg/tests/testthat/misc/helper_makedir.R
@@ -1,4 +1,3 @@
-cat("Helper\n")
 if (!dir.exists("output")) {
   # We have to make sure that directory output is present. If the test is run with
   # Travis CI, then directory output is missing even though output/.gitignore is
