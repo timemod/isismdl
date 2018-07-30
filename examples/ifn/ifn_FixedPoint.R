1ebcdd286c7b02fb99f6f458e748fb789b01b1a9
Rob van Harrevelt <rvanharrevelt@gmail.com>
2018-07-30 15:20:56 +0200

    Added experiments with the FixedPoint package for the ifn model.  Also updated the examples with dfsane en nleqslv.

--- examples/ifn/ifn_FixedPoint.R
+++ examples/ifn/ifn_FixedPoint.R
@@ -1,13 +1,14 @@
 library(isismdl)
-library(BB)
+library(FixedPoint)
 
-# solve the endogenous leads of the IFN model with dfsane
+# solve the endogenous leads of the IFN model with nleqslv
 
 rds_file <- "ifn_basis.rds"
 if (!file.exists(rds_file)) {
   stop("No rds file with model present. Run job ifn_basis.R first")
 }
 ifn <- read_mdl(rds_file)
+
 
 get_endoleads <- function() {
   return (ifn$get_data(names = ifn$get_endo_names(type = "endolead"),
@@ -21,19 +22,16 @@ fun <- function(x) {
   ifn$set_data(endoleads)
   ifn$solve(options = list(mode = "dynamic", report = "none"))
   endoleads2 <- get_endoleads()
-  return (as.numeric(endoleads) - as.numeric(endoleads2))
+  return(as.numeric(endoleads2))
 }
 
 start <-as.numeric(get_endoleads())
 
 print(sum(fun(start)))
 
-ret <- dfsane(start, fun, method = 2, control = list(tol = 1e-7))
-print (ret$convergence)
+ret <- FixedPoint(fun, start,  ConvergenceMetricThreshold = 1e-8, Method = "VEA")
+print(ret)
 
-print(sum(fun(as.numeric(get_endoleads()))))
-
-print(ifn$get_solve_options()$report)
 ifn$solve(options = list(mode = "ratex"))
 
 
--- examples/ifn/ifn_nleqslv.R
+++ examples/ifn/ifn_nleqslv.R
@@ -4,25 +4,25 @@ library(nleqslv)
 # solve the endogenous leads of the IFN model with nleqslv
 
 rds_file <- "ifn_basis.rds"
-if (!file.exists(rds_file)) {	
-    stop("No rds file with model present. Run job ifn_basis.R first")
+if (!file.exists(rds_file)) {
+  stop("No rds file with model present. Run job ifn_basis.R first")
 }
 ifn <- read_mdl(rds_file)
 
 
 get_endoleads <- function() {
-    return (ifn$get_data(names = ifn$get_endo_names(type = "endolead"),
-                         period = ifn$get_period()))
+  return (ifn$get_data(names = ifn$get_endo_names(type = "endolead"),
+                       period = ifn$get_period()))
 }
 
 fun <- function(x) {
-    vnames <- ifn$get_endo_names(type = "endolead")
-    endoleads <- regts(matrix(x, ncol = length(vnames)), names = vnames,
-                               start = start_period(ifn$get_period()))
-    ifn$set_data(endoleads)
-    ifn$solve(options = list(mode = "dynamic", report = "none"))
-    endoleads2 <- get_endoleads()
-    return (as.numeric(endoleads) - as.numeric(endoleads2))
+  vnames <- ifn$get_endo_names(type = "endolead")
+  endoleads <- regts(matrix(x, ncol = length(vnames)), names = vnames,
+                     start = start_period(ifn$get_period()))
+  ifn$set_data(endoleads)
+  ifn$solve(options = list(mode = "dynamic", report = "none"))
+  endoleads2 <- get_endoleads()
+  return (as.numeric(endoleads) - as.numeric(endoleads2))
 }
 
 start <-as.numeric(get_endoleads())
