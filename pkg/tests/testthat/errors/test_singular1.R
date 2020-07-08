268f99cae7758c4bdb7f8be596cd0c26dd39539b
Rob van Harrevelt <rvanharrevelt@gmail.com>
2020-07-08 12:26:33 +0200

    Added a test for the svd analysis for the normal solve procedure.

--- pkg/tests/testthat/errors/test_singular1.R
+++ pkg/tests/testthat/errors/test_singular1.R
@@ -3,17 +3,34 @@ library(testthat)
 
 rm(list = ls())
 
-capture_output(mdl <- isis_mdl("mdl/singular1.mdl", period = "2011"))
+context("singular model 1")
+
+source("../tools/convert_report.R")
+
+mdl <- isis_mdl("mdl/singular1.mdl", period = "2011", silent = TRUE)
 mdl$set_values(0)
 mdl$set_solve_options(dbgopt = c("prifb", "prijac"), maxiter = 1,
                        svdtest_tol = 1e-10)
 
+
 test_that("solve", {
-  expect_warning(expect_output(mdl$solve(options = list(report = "none"))),
-               "Simulation stopped")
+
+  expect_warning(report <- capture.output(mdl$solve()),
+                 "Simulation stopped")
+
+  expect_known_output(cat_report(convert_report(report,
+                                                replace_all_numbers = TRUE)),
+                      "expected_output/singular1_rep1.txt")
+
+  # No output when solve options report ="none":
   mdl$set_values(0)
   expect_silent(expect_warning(mdl$solve(options = list(report = "none",
                                                         svdtest_tol = -1)),
                  "Simulation stopped"))
   expect_identical(mdl$get_solve_status(), "Simulation stopped")
+
+  # There is still  some output of the SVD method when solve options report ="none":
+  mdl$set_values(0)
+  expect_output(expect_warning(mdl$solve(options = list(report = "none")),
+                               "Simulation stopped"))
 })
