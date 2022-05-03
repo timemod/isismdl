1c0c89a7659001c86a04f7e87fed2a7eedab9394
Rob van Harrevelt <R.van.Harrevelt@cpb.nl>
2022-05-03 14:36:42 +0200

    Fixed expected output for R 4.2.0.

--- pkg/tests/testthat/errors/test_numerical_problems.R
+++ pkg/tests/testthat/errors/test_numerical_problems.R
@@ -3,7 +3,7 @@ library(testthat)
 
 rm(list = ls())
 
-update_expected <- TRUE
+update_expected <- FALSE
 
 context("numerical problems")
 
@@ -16,13 +16,15 @@ mdl$set_debug_eqn(TRUE)
 
 test_that("output is correct", {
   report <- convert_report(capture.output(mdl$solve()))
-  #print(report)
-  # there is a difference between the result on Window and Linux, probably
-  # because of a different fortran compiler.
-  # On Windows, Fortran expression 'max(1, NaN)' yields 1 (this is not correct),
-  # on Linux we get NaN, which is correct.
-  expected_output_file <- sprintf("expected_output/numerical_problems_%s.txt",
-                                  .Platform$OS.type)
+  expected_output_file <- "expected_output/numerical_problems.txt"
+  # For R version < 4.2.0, there is a difference between the result on Windows
+  # and Linux, probably because of a different Fortran compiler.
+  # On Windows, Fortran expression 'max(1, NaN)' yields 1 for R version < 4.2.0
+  # (this is not correct). On Linux and for R >= 4.2.0 we get NaN, which is
+  # correct.
+  if (.Platform$OS.type == "windows" && getRversion() < "4.2.0") {
+    expected_output_file <- "expected_output/numerical_problems_windows_before_R_4_2.txt"
+  }
   expect_known_output(cat_report(report), expected_output_file,
                       update = update_expected)
 })
