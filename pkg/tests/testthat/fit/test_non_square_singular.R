08c408f24228f1e964ef4acbe70849718673bd97
Rob van Harrevelt <R.van.Harrevelt@cpb.nl>
2025-10-14 10:33:45 +0200

    Job test_non_square_singular.R can now also run on Ubuntu.

--- pkg/tests/testthat/fit/test_non_square_singular.R
+++ pkg/tests/testthat/fit/test_non_square_singular.R
@@ -5,8 +5,10 @@ rm(list = ls())
 
 update_expected <- FALSE
 
-
 source("../tools/convert_report.R")
+source("../tools/get_platform_variant.R")
+
+platform_variant <- get_platform_variant()
 
 mdl <- isis_mdl("mdl/non_square.mdl", period = 1000, silent = TRUE)
 
@@ -20,11 +22,12 @@ mdl$set_fit(fit)
 mdl$set_rms(rms)
 mdl$set_fit_options(svdtest_tol = 1e-6, dbgopt = "prijac")
 
-test_that("report correct",{
+test_that("report correct", {
   expect_warning(report <- capture.output(mdl$solve()),
                  "Simulation not possible")
   expect_known_output(cat_report(convert_report(report)),
-                                 "expected_output/non_square_singular_rep1.txt",
+                      sprintf("expected_output/non_square_singular_rep1_%s.txt",
+                              platform_variant),
                       update = update_expected)
 })
 
@@ -40,16 +43,19 @@ test_that("zero columns", {
 
   mdl2$set_param(list(r1 = c(2, 2, 0), r2 = c(2, 2, 0)))
   expect_warning(report2 <- capture.output(mdl2$solve()),
-                                      "Simulation not possible")
+                 "Simulation not possible")
   expect_known_output(cat_report(convert_report(report2, replace_all_numbers = TRUE)),
-                      "expected_output/non_square_singular_rep2.txt",
+                      sprintf("expected_output/non_square_singular_rep2_%s.txt",
+                              platform_variant),
                       update = update_expected)
 
-  expect_warning(report3 <- capture.output(mdl2$solve(fit_options =
-                                list(warn_zero_col = TRUE))),
-                 "Simulation not possible")
+  expect_warning(
+    report3 <- capture.output(mdl2$solve(fit_options = list(warn_zero_col = TRUE))),
+    "Simulation not possible"
+  )
   expect_known_output(cat_report(convert_report(report3, replace_all_numbers = TRUE)),
-                      "expected_output/non_square_singular_rep3.txt",
+                      sprintf("expected_output/non_square_singular_rep3_%s.txt",
+                              platform_variant),
                       update = update_expected)
 
   mdl2$set_param(list(r1 = c(1, 0, 0), r2 = c(2, 0, 0)))
@@ -57,7 +63,8 @@ test_that("zero columns", {
                                                         list(warn_zero_col = FALSE))),
                  "Simulation not possible")
   expect_known_output(cat_report(convert_report(report4, replace_all_numbers = TRUE)),
-                      "expected_output/non_square_singular_rep4.txt",
+                      sprintf("expected_output/non_square_singular_rep4_%s.txt",
+                              platform_variant),
                       update = update_expected)
 
   mdl2$set_param(list(r1 = c(1, 1e-8, 0), r2 = c(2, 0, 0)))
@@ -65,6 +72,7 @@ test_that("zero columns", {
                                                         list(warn_zero_col = TRUE))),
                  "Simulation not possible")
   expect_known_output(cat_report(convert_report(report5, replace_all_numbers = TRUE)),
-                      "expected_output/non_square_singular_rep5.txt",
+                      sprintf("expected_output/non_square_singular_rep5_%s.txt",
+                              platform_variant),
                       update = update_expected)
 })
