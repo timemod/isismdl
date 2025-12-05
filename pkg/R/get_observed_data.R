8ad317334ecf163175783688797c99f810750a82
Elanur Akbas <e.akbas@cpb.nl>
2025-12-05 13:16:29 +0100

    dplyr filter gebruiken

--- pkg/R/get_observed_data.R
+++ pkg/R/get_observed_data.R
@@ -1,10 +1,10 @@
 #' @importFrom regts as_data_frame
-#' @importFrom stats filter
+#' @importFrom dplyr filter
 
 get_observed_data = function(data_init) {
   observed_data <- regts::as_data_frame(data_init, format = "long",
                                         name_col = "var", period_col = "period") |>
-    filter(!is.na(.data$value))
+    dplyr::filter(!is.na(.data$value))
 
   return(observed_data)
 }
