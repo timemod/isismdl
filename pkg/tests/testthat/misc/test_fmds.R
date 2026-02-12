1f20374e9f5011185c90fbb0c9ebfc73137c5711
Elanur Akbas <e.akbas@cpb.nl>
2026-02-12 13:19:31 +0100

    check pkg error actual msg

--- pkg/tests/testthat/misc/test_fmds.R
+++ pkg/tests/testthat/misc/test_fmds.R
@@ -352,7 +352,7 @@ test_that("fill_mdl_data_solve reports multiple non-NA solve variables", {
   output <- capture.output({
     expect_error(
       mdl$fill_mdl_data_solve(solve_df = solve_df, report = "no"),
-      regexp = "Some solve variables are not NA"
+      regexp = "Some solve variables are not NA .... See the table above."
     )
   })
 
